8120d3e05eb436e8ce2d8186d0981e7f1b4896fb
Millmoss <omelliff@yahoo.com>
2019-02-08 19:44:26 -0500

    Boat can't go up ramps???

--- AboatBoats/Assets/Boat.cs
+++ AboatBoats/Assets/Boat.cs
@@ -8,6 +8,7 @@ public class Boat : MonoBehaviour
 	public float turnPower;
 	public float speedLimit;
 	public float buoyancy;
+	public float seaHeightMod;
 	public GameObject motor;
 	public GameObject pin;
 	public GameObject spinny;
@@ -22,6 +23,7 @@ public class Boat : MonoBehaviour
 	private float zRotationMod;
 	private Vector3 positionChange;
 	private bool onRamp;
+	private float seaHeight;
 
     void Start()
     {
@@ -39,19 +41,17 @@ public class Boat : MonoBehaviour
 	
     void Update()
     {
-		positionChange = transform.position - positionChange;
-
+		print(velocity);
+		seaHeight = sea.transform.position.y + seaHeightMod;
 		velocity = Vector3.zero;
 		control();
-		if (transform.position.y <= sea.transform.position.y)
+		if (transform.position.y <= seaHeight)
 			water();
 		gravity();
 
-		velocity += speed;
-		
-		velocity += waterPlusGravity;
-
-		positionChange = transform.position;
+		//boatBody.velocity += speed * Time.deltaTime;
+		//boatBody.velocity = Vector3.ClampMagnitude(boatBody.velocity, speedLimit);
+		//velocity += waterPlusGravity;
 	}
 
 	void FixedUpdate()
@@ -59,27 +59,23 @@ public class Boat : MonoBehaviour
 		phys();
 
 		speed = Vector3.ClampMagnitude(speed, speedLimit);
-		transform.position += velocity * Time.deltaTime;
+		//transform.position += velocity * Time.deltaTime;
 
-		float yPos = Mathf.Lerp(transform.position.y, sea.transform.position.y, Mathf.Abs(24 - velocity.magnitude) / 10 * Time.deltaTime);
-		//print(velocity.magnitude);
-		
-		transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
-
-		transform.Rotate(new Vector3(0, -speed.magnitude * 0.05f * Vector3.SignedAngle(transform.forward, motor.transform.up, transform.up) * Time.deltaTime));
+		//transform.Rotate(new Vector3(0, -speed.magnitude * 0.05f * Vector3.SignedAngle(transform.forward, motor.transform.up, transform.up) * Time.deltaTime));
+		transform.Rotate(new Vector3(0, -boatBody.velocity.magnitude * 0.05f * Vector3.SignedAngle(transform.forward, motor.transform.up, transform.up) * Time.deltaTime));
 	}
 
 	private void control()
 	{
 		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.I))
 		{
-			if (transform.position.y <= sea.transform.position.y)
-				speed += motor.transform.up * hossPower * Time.deltaTime;
+			if (transform.position.y <= seaHeight)
+				boatBody.velocity += motor.transform.up * hossPower * Time.deltaTime;
 		}
 		else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.K))
 		{
-			if (transform.position.y <= sea.transform.position.y)
-				speed -= motor.transform.up * hossPower * Time.deltaTime / 10;
+			if (transform.position.y <= seaHeight)
+				boatBody.velocity -= motor.transform.up * hossPower * Time.deltaTime / 10;
 		}
 		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.J))
 		{
@@ -99,35 +95,47 @@ public class Boat : MonoBehaviour
 
 	private void water()	//this represents the water's effects on the boat, converting semi-forward speed into full forward speed over time
 	{
-		Vector3 bFor = Vector3.Project(speed, transform.forward);
+		/*Vector3 bFor = Vector3.Project(speed, transform.forward);
 		Vector3 oldVelocity = speed;
 		speed -= speed * Time.deltaTime / 1.2f;
-		speed += bFor * Time.deltaTime / (1.4f + oldVelocity.magnitude - bFor.magnitude);
+		speed += bFor * Time.deltaTime / (0.7f + oldVelocity.magnitude - bFor.magnitude);
 
 		xRotationMod = -bFor.magnitude / 1.55f;
 		zRotationMod = (Vector3.SignedAngle(transform.forward, oldVelocity, transform.up) / 7f) * Mathf.Clamp01(oldVelocity.magnitude / 5);
 
-		waterPlusGravity += Vector3.up * buoyancy * Time.deltaTime * (sea.transform.position.y - transform.position.y) * 3f;
+		waterPlusGravity += Vector3.up * buoyancy * Time.deltaTime * (seaHeight - transform.position.y) * 3f;*/
+
+		Vector3 bFor = Vector3.Project(boatBody.velocity, transform.forward);
+		Vector3 oldVelocity = boatBody.velocity;
+		boatBody.velocity -= boatBody.velocity * Time.deltaTime / 1.2f;
+		boatBody.velocity += bFor * Time.deltaTime / (0.7f + oldVelocity.magnitude - bFor.magnitude);
+
+		xRotationMod = -bFor.magnitude / 1.55f;
+		zRotationMod = (Vector3.SignedAngle(transform.forward, oldVelocity, transform.up) / 7f) * Mathf.Clamp01(oldVelocity.magnitude / 5);
+
+		boatBody.velocity += Vector3.up * buoyancy * Time.deltaTime * (seaHeight - transform.position.y) * 3f;
+
+		//TOTO : Add velocity slowdown to physics.
 	}
 
 	private void gravity()
 	{
-		waterPlusGravity -= Vector3.up * 9.8f * Time.deltaTime;
-		if (waterPlusGravity.y < -1f && onRamp)
-		{
-			waterPlusGravity = new Vector3(waterPlusGravity.x, -1, waterPlusGravity.z);
-			//print("AHAFDSG");
-		}
+		if (boatBody.velocity.y > 0)
+			boatBody.velocity -= Vector3.up * Mathf.Lerp(25f, 9.8f, (5 - boatBody.velocity.y) / 5) * Time.deltaTime;
+		else
+			boatBody.velocity -= Vector3.up * 9.8f * Time.deltaTime;
 	}
 
 	private void phys()
 	{
-		boatBody.velocity = Vector3.zero;
-		if (transform.position.y <= sea.transform.position.y)
+		if (transform.position.y <= seaHeight)
 		{
 			boatBody.angularVelocity = Vector3.Lerp(boatBody.angularVelocity, Vector3.zero, 0.5f);
 			transform.rotation = Quaternion.Slerp(boatBody.rotation, Quaternion.Euler(xRotationStart + xRotationMod, boatBody.rotation.eulerAngles.y, zRotationStart + zRotationMod), 0.1f);
 		}
+
+		Vector3 clampVec = Vector3.ClampMagnitude(new Vector3(boatBody.velocity.x, 0, boatBody.velocity.z), speedLimit);
+		boatBody.velocity = new Vector3(clampVec.x, boatBody.velocity.y, clampVec.z);
 	}
 
 	void OnCollisionEnter(Collision c)
