8a093f1f3c61495da491f3310d15dc5c565ec7dc
lessuknow <chenc18@rpi.edu>
2019-02-12 15:14:39 -0500

    Water script

---
+++ AboatBoats/Assets/Scripts/Collidable_Water.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Assuming there's a tag for the boat
+[RequireComponent(typeof(MeshCollider))]
+public class Collidable_Water : MonoBehaviour
+{
+    private static string player_tag = "Player";
+    public Water wt;
+    private MeshCollider disable_onhit;
+
+    private void Start()
+    {
+        disable_onhit = GetComponent<MeshCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == player_tag)
+        {
+            wt.Sink();
+            disable_onhit.enabled = false;
+        }
+    }
+}
---
+++ AboatBoats/Assets/Scripts/Water.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public float decrease_rate = 0.0f;
+    private bool decrease = false;
+    public float bottom;
+    private GameObject wtr;
+
+
+    private void Start()
+    {
+        wtr = gameObject;
+    }
+
+    public void Sink()
+    {
+        decrease = true;
+    }
+
+    private void Update()
+    {
+        if(decrease)
+        {
+            if(wtr.transform.position.y > bottom)
+            {
+                wtr.transform.position = new Vector3(
+                    wtr.transform.position.x,
+                    wtr.transform.position.y - decrease_rate * Time.deltaTime,
+                    wtr.transform.position.z);
+            }
+        }
+    }
+
+
+}
